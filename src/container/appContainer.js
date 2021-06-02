2f9d7fae1d5a40dbd4d06656d9ea1e5b68bc6103
RupasreeJilumudi <77183679+RupasreeJilumudi@users.noreply.github.com>
2021-06-02 18:49:34 +0530

    Delete appContainer.js

--- src/container/appContainer.js
+++
@@ -1,7 +0,0 @@
-import {connect} from 'react-redux';
-//import App from '../App'
-import App from '../component/App'
-const mapStateToProps=state=>({
-    getSelectedValue: state.event.selectValue,
- });
-export default connect(mapStateToProps)(App);
